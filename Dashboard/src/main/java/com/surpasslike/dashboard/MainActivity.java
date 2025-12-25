48ecaa75e59c4b58c596902b2f93c5ee4433d19a
like-ex <like@xtourtec.com>
2025-12-25 18:30:04 +0800

    完成客户端,可以正确连接成功并显示模拟车速

--- Dashboard/src/main/java/com/surpasslike/dashboard/MainActivity.java
+++ Dashboard/src/main/java/com/surpasslike/dashboard/MainActivity.java
@@ -1,6 +1,14 @@
 package com.surpasslike.dashboard;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +16,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.surpasslike.androidlearning.IVehicleCallback;
+import com.surpasslike.androidlearning.IVehicleData;
+
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "客户端 MainActivity";
+    private TextView tvCurrentSpeed;
+    private IVehicleData mVehicleDataService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,36 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        tvCurrentSpeed = findViewById(R.id.tv_current_speed);
+
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName("com.surpasslike.androidlearning", "com.surpasslike.androidlearning.VehicleService"));
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
     }
+
+    private IVehicleCallback mCallback = new IVehicleCallback.Stub() {
+        @Override
+        public void onSpeedChanged(int speed) throws RemoteException {
+            runOnUiThread(() -> tvCurrentSpeed.setText("当前车速：" + speed + " km/h"));
+        }
+    };
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.d(TAG, "连接成功");
+            mVehicleDataService = IVehicleData.Stub.asInterface(service);// 这里的service就是VehicleService里面onBind里面的mBinder
+            try {
+                mVehicleDataService.registerVehicleCallback(mCallback);
+            } catch (RemoteException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.d(TAG, "连接断开");
+            mVehicleDataService = null;
+        }
+    };
 }
