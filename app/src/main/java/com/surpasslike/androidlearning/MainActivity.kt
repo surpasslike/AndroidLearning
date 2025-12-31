5dc2551cd0f1c3133a2fdfb0745d40135b0e1d01
like-ex <like@xtourtec.com>
2025-12-31 14:49:57 +0800

    动态注册BroadcastReceiver

--- app/src/main/java/com/surpasslike/androidlearning/MainActivity.kt
+++ app/src/main/java/com/surpasslike/androidlearning/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.surpasslike.androidlearning
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var networkReceiver: BroadcastReceiver? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // 创建意图的过滤器,指定接收的广播类型
+        // 告诉门铃系统："我只关心网络变化这件事，其他事不用通知我。"
+        val filter = IntentFilter()
+        filter.addAction("android.net.conn.CONNECTIVITY_CHANGE") // 只接收网络广播
+
+        // 门铃响了之后你要做的事——去检查网络状态
+        networkReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                checkNetworkStatus()
+            }
         }
+
+        // 注册广播接收器,把门铃系统安装并启用。只有注册了，才能收到广播。
+        registerReceiver(networkReceiver, filter)
+    }
+
+    private fun checkNetworkStatus() {
+        // 检查网络状态的逻辑
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // 取消注册** Activity 销毁时，**拆掉门铃系统**
+        unregisterReceiver(networkReceiver)
     }
 }
